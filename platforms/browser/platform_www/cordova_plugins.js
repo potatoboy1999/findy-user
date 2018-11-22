d7f0ece9e80b36ae0d08e00b066a5739a37bd2d1
PotatoBoy <PotatoBoy@bitbucket.org>
2018-11-22 11:14:50 -0500

    Testing geo

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-compat": "1.2.0",
+    "cordova-plugin-geolocation": "4.0.1",
+    "cordova-plugin-whitelist": "1.3.3"
+}
+// BOTTOM OF METADATA
+});
