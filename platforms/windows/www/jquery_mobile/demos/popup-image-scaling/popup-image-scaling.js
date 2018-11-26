9ce476558ab75a4a16cb688ef86bb6fa3a26dc36
PotatoBoy <PotatoBoy@bitbucket.org>
2018-11-26 13:59:44 -0500

    fix size

--- platforms/windows/www/jquery_mobile/demos/popup-image-scaling/popup-image-scaling.js
+++
@@ -1,8 +0,0 @@
-﻿$( document ).on( "pagecreate", function() {
-	$( ".photopopup" ).on({
-		popupbeforeposition: function() {
-			var maxHeight = $( window ).height() - 60 + "px";
-			$( ".photopopup img" ).css( "max-height", maxHeight );
-		}
-	});
-});
